8757eb8d5b19b2e9d52d6fcb125b57eeb4e9d035
rkeng <rkeng@ucsd.edu>
2019-05-20 21:56:19 -0700

    file upload checkpoint

--- functions/index.js
+++ functions/index.js
@@ -134,5 +134,74 @@ app.get('/profile', (req, res) => {
    })
 })
 
+// generate 3 prompts once a week
+// https://firebase.google.com/docs/functions/schedule-functions?hl=en
+
+// submit photos to firebase storage
+// https://firebase.google.com/docs/storage/web/start
+// let storage = firebase.storage();
+var path = require('path');
+var os = require('os');
+var fs = require('fs');
+var Busboy = require('busboy');
+
+app.post('/submit-form', (req, res) => {
+   const busboy = new Busboy({ headers: req.headers });
+   const tmpdir = os.tmpdir();
+
+   // This object will accumulate all the fields, keyed by their name
+   const fields = {};
+
+   // This object will accumulate all the uploaded files, keyed by their name.
+   const uploads = {};
+
+   // This code will process each non-file field in the form.
+   busboy.on('field', (fieldname, val) => {
+      // TODO(developer): Process submitted field values here
+      console.log(`Processed field ${fieldname}: ${val}.`);
+      fields[fieldname] = val;
+   });
+
+   const fileWrites = [];
+
+   // This code will process each file uploaded.
+   busboy.on('file', (fieldname, file, filename) => {
+      // Note: os.tmpdir() points to an in-memory file system on GCF
+      // Thus, any files in it must fit in the instance's memory.
+      console.log(`Processed file ${filename}`);
+      const filepath = path.join(tmpdir, filename);
+      uploads[fieldname] = filepath;
+
+      const writeStream = fs.createWriteStream(filepath);
+      file.pipe(writeStream);
+
+      // File was processed by Busboy; wait for it to be written to disk.
+      const promise = new Promise((resolve, reject) => {
+         file.on('end', () => {
+            writeStream.end();
+         });
+         writeStream.on('finish', resolve);
+         writeStream.on('error', reject);
+      });
+      fileWrites.push(promise);
+   });
+
+   // Triggered once all uploaded files are processed by Busboy.
+   // We still need to wait for the disk writes (saves) to complete.
+   busboy.on('finish', () => {
+      Promise.all(fileWrites).then(() => {
+         // TODO(developer): Process saved files here
+         for (const name in uploads) {
+            const file = uploads[name];
+            fs.unlinkSync(file);
+         }
+         res.send();
+      });
+   });
+
+   busboy.end(req.rawBody);
+})
+
+// submit evaluation for a submission
 
 exports.app = functions.https.onRequest(app);
