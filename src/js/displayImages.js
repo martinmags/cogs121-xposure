d92a870d58803ad5ed086a724bdc67a053686ac9
Martin Magsombol <martinmagsombol@martins-mini.home>
2019-05-28 22:26:10 -0700

    issue with displaying submission

--- src/js/displayImages.js
+++ src/js/displayImages.js
@@ -8,13 +8,5 @@ firebase.initializeApp({
 var db = firebase.firestore();
 
 $(document).ready( () => {
-  db.collection("submissions").get(). then((querySnapshot) => {
-    let imageArray = [];
-    querySnapshot.forEach( (doc) =>{
-      imageArray.push(doc.data().file);
-    });
-    console.log(imageArray);
-  }).catch((error) => {
-    console.error("Error: ", error);
-  });
+  
 });
